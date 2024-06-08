690113143048c0edc0d3daf2ad271137e51e4513
DM-Alec <157842383+DM-Alec@users.noreply.github.com>
2024-06-08 11:38:12 -0500

    Add files via upload

---
+++ elifstate.py
@@ -0,0 +1,12 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+#this is the same as if statements, but this is better code for bigger code
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x is equal to y")
+
+
