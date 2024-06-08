8e3dcfe29f66cb2edfc139ff202d70f7620fc369
DM-Alec <157842383+DM-Alec@users.noreply.github.com>
2024-06-08 11:41:34 -0500

    Add files via upload

---
+++ orstate.py
@@ -0,0 +1,14 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+# or statemetns can add multiple if or elif statements that have same conclusions to simplify code
+#if x < y or x > y:
+ #   print("x is not equal to y")
+#else:
+  #  print("x is equal to y")
+
+  # not equal can simplify code further, can be reversed for == instead of != but switch print statements
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
