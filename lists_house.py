ad50a06e6ab72c0c4d3ac7e5ec40ca0ddc0be2e9
DM-Alec <157842383+DM-Alec@users.noreply.github.com>
2024-06-09 14:17:34 -0500

    Add files via upload

---
+++ lists_house.py
@@ -0,0 +1,19 @@
+students = ["Hermoine", "Harry", "Ron"]
+
+print(students[0]) # saves/indexes var to list matrix
+print(students[1])
+print(students[2])
+
+#uses loop to print list, or itterate strings within list
+students = ["Hermoine", "Harry", "Ron"]
+
+for student in students:
+    print(student) #will print all students in list
+
+#uses length "len" to check length of list called student
+students = ["Hermoine", "Harry", "Ron"]
+
+for i in range(len(students)):
+    print(i + 1, students[i])
+
+
